a1f8850c3fe18c9427d2a342f46400ec855f198e
Nargonath <jonas.pauthier@gmail.com>
2017-06-30 21:10:02 +0200

    Updated footer information

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>Je suis le footer</footer>
+    <footer>
+      Created with &hearts; by <a href="https://twitter.com/jonas_pauthier">@Jonas_Pauthier</a> (code) and <a href="http://jeremy-fermine.com/">Jeremy Fermine</a> (design)
+    </footer>
   );
 }
 
