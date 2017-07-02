c08051e672fe1de57e210fad968402d8b67f5901
Nargonath <jonas.pauthier@gmail.com>
2017-07-02 23:57:00 +0200

    Added more tests to Footer

--- src/components/Footer/Footer.test.js
+++ src/components/Footer/Footer.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
 import Footer from './';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Footer />, div);
+});
+
+it('displays as a footer tag', () => {
+  const wrapper = shallow(<Footer />);
+  expect(wrapper.find('footer')).toHaveLength(1);
+});
+
+it('links to both authors', () => {
+  const wrapper = shallow(<Footer />);
+  expect(wrapper.find('a')).toHaveLength(2);
 });
